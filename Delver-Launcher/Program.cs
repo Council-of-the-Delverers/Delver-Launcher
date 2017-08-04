90c8908e8a0491c0c557a1f69304390921c7a9fa
ElPumpo <samo_kol@hotmail.com>
2017-08-04 13:41:48 +0200

    Initial push to Github

---
+++ Delver-Launcher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Delver_Launcher {
+    class Program {
+        static string ArgList = null;
+        static void Main(string[] args) {
+            foreach (var arg in args) {
+                ArgList = ArgList + arg + " ";
+            }
+
+            Console.WriteLine(ArgList);
+
+
+            Process process = new Process();
+            process.StartInfo.FileName = "java";
+            process.StartInfo.Arguments = "-jar Delver.jar " + ArgList;
+
+            process.Start();
+        }
+        
+
+    }
+}
